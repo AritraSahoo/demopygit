9a16a01932d5b6b4c4f0e075c51d9f580d93c9b1
Aritra <artransec1230@gmail.com>
2020-08-31 12:43:10 +0530

    test

--- demo.py
+++ demo.py
@@ -1,1 +1,13 @@
-print("Hello")
+import  mysql;
+from mysql.connector import connect
+
+mydb = connect(host="localhost", user ="root", password="root");
+
+myCursor = mydb.cursor()
+myCursor.execute("use new_schema")
+myCursor.execute("select * from new_table")
+
+result = myCursor.fetchmany(2)
+
+for i in result:
+    print(i)
